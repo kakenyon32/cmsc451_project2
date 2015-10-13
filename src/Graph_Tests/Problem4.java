2f88b3198cbcc678c0bf1ae88d66832d719e3c32
Kent Kenyon <kakenyon32@gmail.com>
2015-10-13 12:45:53 -0400

    Simplified cycle detection in Problem4.java

--- src/Graph_Tests/Problem4.java
+++ src/Graph_Tests/Problem4.java
@@ -32,59 +32,27 @@ public class Problem4 {
         });
         start.setColor("GRAY");
         Q.add(start);
-        ArrayList<Edge> possible = new ArrayList<>();
         while (!Q.isEmpty()) {
             if (!G.adj.get(Q.peek()).isEmpty()) {
                 G.adj.get(Q.peek()).stream().forEach((e) -> {
-                    if (e.get_v().getColor().equals("WHITE")) {
-                        e.get_v().setColor("GRAY");
-                        e.get_v().setDiscovery(Q.peek().getDiscovery() + 1);
-                        e.get_v().setParent(Q.peek());
-                        Q.add(e.get_v());
-                    } 
-                    else if (e.get_v().getColor().equals("BLACK")) {
-                        // because this is a directed graph, if "v" has been
-                        // visited previously, and is at a depth less than the
-                        // current "u", then there may be a cycle.
-                        
-                        cycle = true;
-                        
-                        //if (e.get_v().getDiscovery() < Q.peek().getDiscovery())
-                        //    possible.add(e);
+                    switch (e.get_v().getColor()) {
+                        case "WHITE":
+                            e.get_v().setColor("GRAY");
+                            e.get_v().setDiscovery(Q.peek().getDiscovery() + 1);
+                            e.get_v().setParent(Q.peek());
+                            Q.add(e.get_v());
+                            break;
+                        case "BLACK": // if this case is true, then there is a cycle
+                            cycle = true;
+                            break;
                     }
                 });
             }
             Q.peek().setColor("BLACK");
             Q.remove();
         }
-        
-        // now that the BFS is complete, test "possible" candidates for 
-        // common ancestory in reverse order.
-        /*
-        possible.stream().forEach((e) -> {
-            ArrayList<Vertex> list = Traverse(e.get_u());
-            if (list.contains(e.get_v())) cycle = true;
-            else G.adj.get(e.get_v()).stream().forEach((e2) -> {
-                    if (list.contains(e2.get_v())) cycle = true;
-                });
-        });
-        */
     }
     
     /** @return whether a cycle exists in the BFS */
     public boolean getCycle() { return cycle; }
-    
-    /**
-     * Traverses the ancestor path of a given Vertex
-     * @param u the Vertex to be traced
-     * @return a list of ancestors to a root Vertex
-     */
-    private ArrayList<Vertex> Traverse(Vertex u) {
-        ArrayList<Vertex> ancestors = new ArrayList<>();
-        while (u.getParent() != null) {
-            ancestors.add(u.getParent());
-            u = u.getParent();
-        }
-        return ancestors;
-    }
 }
